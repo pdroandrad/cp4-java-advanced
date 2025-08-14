8c89827d636b6a05805d2ff5a836e3fb7d7c189c
andremarko <151890840+andremarko@users.noreply.github.com>
2025-08-14 18:46:47 -0300

    chore(dto): adiciona exemplo de produto, com descrição para documentação e persistência

--- src/main/java/com/example/MiniMercado/dto/ProdutoRequestDto.java
+++ src/main/java/com/example/MiniMercado/dto/ProdutoRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.MiniMercado.dto;
 
 import com.example.MiniMercado.utils.Tamanho;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -12,18 +13,38 @@ import lombok.*;
 
 public class ProdutoRequestDto {
 
+    @Schema(
+            description="Nome do produto",
+            example="Sal Cisne Refinado"
+    )
     @NotBlank(message = "O nome do produto é obrigatório!")
     private String nome;
 
+    @Schema(
+            description="Categoria do produto",
+            example="Temperos e Condimentos"
+    )
     @NotBlank(message = "O tipo do produto é obrigatório!")
     private String tipo;
 
+    @Schema(
+            description="Setor do produto no mercado",
+            example="Alimentos básicos"
+    )
     @NotBlank(message = "O setor do produto é obrigatório!")
     private String setor;
 
+    @Schema(
+            description="Tamanho do produto (em peso ou volume)",
+            example="1kg"
+    )
     @NotNull(message = "O tamanho do produto é obrigatório!")
     private Tamanho tamanho;
 
+    @Schema(
+            description="Preço do produto",
+            example="4.29"
+    )
     @NotNull(message = "O preço do produto é obrigatório!")
     private Double preco;
 }
